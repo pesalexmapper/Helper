3a4f3d84573efe3f1719aebfefa6d8317bc3a735
Eduardo Silva <p.eduardo.silva@outlook.com>
2020-03-14 14:48:41 -0300

    Added DescriptionArray, GetDescription by value int and GetValue by Description

--- src/PESALEXMapper.Helper/PESALEXMapper.Helper.Tests/EnumUtilUnitTest.cs
+++ src/PESALEXMapper.Helper/PESALEXMapper.Helper.Tests/EnumUtilUnitTest.cs
@@ -11,7 +11,16 @@ namespace PESALEXMapper.Helper.Tests
         public void DescriptionList_Count_4()
         {
             var result = EnumUtil.DescriptionList<EnumValue>();
+            Assert.AreEqual("1ª name", result[0]);
             Assert.AreEqual(4, result.Count);
+        }
+
+        [TestMethod]
+        public void DescriptionArray_Count_4()
+        {
+            var result = EnumUtil.DescriptionArray<EnumValue>();
+            Assert.AreEqual("1ª name", result[0]);
+            Assert.AreEqual(4, result.Length);
         }
 
         [TestMethod]
@@ -22,10 +31,31 @@ namespace PESALEXMapper.Helper.Tests
         }
 
         [TestMethod]
+        public void GetDescription_FooBarName_3rdName()
+        {
+            var result = EnumUtil.GetDescription<EnumValue>("FooBar");
+            Assert.AreEqual("3ª name", result);
+        }
+
+        [TestMethod]
         public void GetDescriptionOfType_EnumValue_Values()
         {
             var result = EnumUtil.GetDescriptionOfType<EnumValue>();
             Assert.AreEqual("Values", result);
+        }
+
+        [TestMethod]
+        public void GetDescription_ByValueEquals3_4ªname()
+        {
+            var result = EnumUtil.GetDescription<EnumValue>(3);
+            Assert.AreEqual("4ª name", result);
+        }
+
+        [TestMethod]
+        public void GetValue_BydescriptionEquals4ªname_ValueEquals3()
+        {
+            var result = EnumUtil.GetValue<EnumValue>("4ª name");
+            Assert.AreEqual(3, result);
         }
     }
 }
